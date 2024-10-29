aa074f7d829e3bf4a31eaa03b81c08ef9e19cb1a
Adam <adam>
2024-10-29 11:42:50 -0400

    Initial Commit

---
+++ DimeADozen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DimeADozen
+//
+//  Created by Adam J Mosher on 10/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DimeADozen/DimeADozenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimeADozenApp.swift
+//  DimeADozen
+//
+//  Created by Adam J Mosher on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DimeADozenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
