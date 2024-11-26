8b47ccc23ae6e91345fc83a23966d555cf2fd1da
Adam <adam.mosher1@yahoo.com>
2024-11-26 01:03:47 -0500

    Added a "Get Started" Page as well as a page dedicated to inputing the user's budget. Also added a test MainPage() just to make sure the workflow is solid before I do anything else.

--- DimeADozen/ContentView.swift
+++ DimeADozen/ContentView.swift
@@ -4,19 +4,87 @@
 //
 //  Created by Adam J Mosher on 10/29/24.
 //
+//Notes as of 11/26: I HATE BUTTONS!!!!! - Adam :)
+
+
+// TODO:
+// Make a button to bring the user to the MainPage()
+// Make the same button bring over budget
+// Stylize the MainPage() (obviously why tf am I adding this?)
 
 import SwiftUI
 
+// Made by Adam Mosher
+// Edited by
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("This is a test.")
+        NavigationStack {
+            ZStack {
+                Color.green
+                    .edgesIgnoringSafeArea(.all)
+                
+                Text("Welcome to DimeADozen.")
+                    .font(.title)
+                    .bold()
+                
+                VStack {
+                    Spacer()
+                    
+                    NavigationLink(destination: SetUp()) {
+                        Text("Get Started")
+                    }
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: .infinity)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    .padding(.horizontal)
+                }
+            }
         }
-        .padding()
+    }
+}
+
+// Made by Adam Mosher
+// Edited by
+struct SetUp: View {
+    @State private var budgetInput: String = ""
+    @State private var budget: Int? = nil
+    @State private var isNavigating: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.green.edgesIgnoringSafeArea(.all)
+
+                VStack(spacing: 20) {
+                    Text("Let us set up a budget.")
+                        .padding()
+                        .font(.title)
+                        .bold()
+
+                    Text("You can always change this later.")
+                        .padding()
+
+                    TextField("Enter your budget", text: $budgetInput)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.numberPad)
+                        .frame(width: 300)
+                        .padding()
+
+                }
+            }
+        }
+    }
+}
+
+// Made by Adam Mosher
+// Edited by
+struct MainPage: View {
+    var body: some View {
+        Text("Test")
     }
 }
 
